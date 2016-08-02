d4a73585bf8c4346c1f0c9747c56a174f0c35499
Pigggger <296056428@qq.com>
2016-08-02 23:13:26 +0800

    Uncomplished

---
+++ FlappyBird/Assets/Scrips/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Transform firstBg;
+
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
---
+++ FlappyBird/Assets/Scrips/Pipe.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pipe : MonoBehaviour {
+
+    private void Start()
+    {
+        RandomGeneratePosition();
+    }
+
+    public void RandomGeneratePosition()
+    {
+        float posY = Random.Range(-0.61f, -0.16f);
+        transform.localPosition = new Vector3(transform.localPosition.x, posY, transform.localPosition.z);
+       // Debug.Log(posY);
+    }
+}
