45ac496b62216ba042cd20eddf15605c72c10e66
Pigggger <296056428@qq.com>
2016-08-04 15:52:36 +0800

    init

---
+++ Roll a Ball/Assets/Scrips/Food.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour
+{
+
+    public float speed;
+	// Update is called once per frame
+	void Update ()
+	{
+        transform.Rotate(new Vector3(45, 45, 45) * Time.deltaTime * speed);
+	}
+}
---
+++ Roll a Ball/Assets/Scrips/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private int count;
+    private Rigidbody rb;
+
+
+	void Start ()
+	{
+	    rb = GetComponent<Rigidbody>();
+	    winText.text = "";
+	    SetCountText();
+
+	}
+
+    void FixedUpdate()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalMove,0,verticalMove);
+
+        rb.AddForce(movement*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        if (other.CompareTag("Food"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+
+        if (count == 15)
+        {
+            winText.text = "YOU WIN!";
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count:" + count;
+    }
+}
