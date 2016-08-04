30893cc593766f0f8380b44c9d575cd31c3de486
Pigggger <296056428@qq.com>
2016-08-04 16:12:15 +0800

    init

---
+++ FlappyBird/Assets/Scrips/Bird.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour
+{
+
+    private float timer = 0;
+    public int frameCount = 0; //帧数计时器
+    public int frameNumber = 10;//每秒10帧
+    public float speed = 5.0f;
+
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(speed,0,0);
+    }
+
+    // Update is called once per frame
+	void Update ()
+	{
+	    timer += Time.deltaTime;
+	    if (timer >= 1.0f/frameNumber)
+	    {
+	        frameCount++;
+	        int frameIndex = frameCount%3;
+            GetComponent<Renderer>().material.SetTextureOffset("_MainTex",new Vector2(0.3333f*frameIndex,0));
+	        timer -= 1.0f/frameNumber;
+	    }
+	}
+}
---
+++ FlappyBird/Assets/Scrips/MoveTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTrigger : MonoBehaviour
+{
+    public Pipe pipe1;
+    public Pipe pipe2;
+    public Transform currentBg;
+
+    public void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "Player")
+        {
+            Transform firstBg= GameManager.Instance.firstBg;
+            currentBg.position = new Vector3(firstBg.position.x+10,currentBg.position.y,currentBg.position.z);
+            GameManager.Instance.firstBg = currentBg;
+
+            //更新管道位置
+            pipe1.RandomGeneratePosition();
+            pipe2.RandomGeneratePosition();
+
+        }
+    }
+}
